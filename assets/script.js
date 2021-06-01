1982df3871f720db138d868b41211f13d67805fa
yonathansiregar <alfrenanday@gmail.com>
2021-06-02 00:01:58 +0700

    Create navbar and disable inspect element using JS

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-document.onkeydown = function(e) {
-    if(event.keyCode == 123) return false;
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false;
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
-}
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+document.addEventListener('keydown', function(e) {
+   if (event.keyCode == 123) return false;
+   if (e.ctrlKey && e.shiftKey) return false;
+   if (event.ctrlKey && event.keyCode == 85) return false;
+});
